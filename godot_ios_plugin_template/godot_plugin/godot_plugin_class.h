b4c2c71baf2243ee8f88a9b11af82fed274cc3e9
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2025-03-30 07:13:03 +0900

    Update godot_plugin_class.h

--- godot_ios_plugin_template/godot_plugin/godot_plugin_class.h
+++ godot_ios_plugin_template/godot_plugin/godot_plugin_class.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "core/object.h"
+#include "core/object/object.h"
+#include "core/object/class_db.h"
 
 class PluginClass : public Object {
     GDCLASS(PluginClass, Object);
